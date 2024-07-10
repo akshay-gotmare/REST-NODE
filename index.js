f8191c28528f5794205d5df8cc22250f62469833
Akshay Gotmare <akshaygotmare4@gmail.com>
2024-07-10 17:07:06 +0530

    Realtime API

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const fs = require("fs");
+const PORT = 20024;
+const users = require("./MOCK_DATA.json");
+const { json } = require("body-parser");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    console.log("Hello");
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const body = req.body;
+    const updatedUsers = users.map((user) => {
+      if (user.id === id) {
+        user = { id: user.id, ...body };
+      }
+      return user;
+    });
+    fs.writeFile(
+      "./MOCK_DATA.json",
+      JSON.stringify(updatedUsers),
+      (err, data) => {
+        return res.json({ status: "Success", id: id });
+      }
+    );
+  })
+  .delete((req, res) => {
+    const id = req.params.id;
+    res.json({ status: "Pending..." });
+  });
+
+app.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+  users.push({ id: users.length + 1, ...newUser });
+  console.log(users.length);
+
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
+});
+
+// app.get("/api/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const user = users.find((user) => user.id === id);
+//   console.log("Hello");
+//   return res.json(user);
+// });
+// app.patch("/api/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   res.json({ status: "Pending..." });
+// });
+// app.delete("/api/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   res.json({ status: "Pending..." });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server listening at PORT ${PORT}`);
+});
